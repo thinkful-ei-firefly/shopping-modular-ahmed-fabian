072e8fe406441b1b4ba25fbf04e80beece83be18
Fabian Lema <fabianlemac@gmail.com>
2019-07-17 11:04:28 -0500

    validateName and create functions

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,10 +1,18 @@
 
 
 const Item = (function() {
+	const validateName = function(name){
+		if (!name) throw new TypeError("Name does not exist.");
+	}
+
+	const create = function(name){
+		return {id: cuid(), name, checked:false}
+	}
 
 // const foo = 'bar';
 
-return {
-
-};
+	return {
+		validateName,
+		create
+	};
 }());
