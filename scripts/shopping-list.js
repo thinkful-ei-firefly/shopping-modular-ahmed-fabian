c94bf69569a2291e1189fd6ed66b5af986053bc9
Ahmed <ahmed.max7586@gmail.com>
2019-07-17 12:15:40 -0400

    update shoppingList to use the Item.create method

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -57,7 +57,15 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+      Item.validateName(itemName);
+      // Item.create (itemName)
+      store.items.push(Item.create(itemName));
+      render();
+    } catch (error) {
+      console.log('Cannot add item: {error.message}');
+    }
+    // store.items.push({ id: cuid(), name: itemName, checked: false });
   }
   
   function handleNewItemSubmit() {
